297a4b68bfda819f365fb09bb94da81bd16e3a6e
Gomguk <kaioga4187@gmail.com>
2026-03-05 13:08:36 +0900

    apply discount text

--- app/src/main/java/com/gomguk/kirly/ui/main/ProductAdapter.kt
+++ app/src/main/java/com/gomguk/kirly/ui/main/ProductAdapter.kt
@@ -1,13 +1,16 @@
 package com.gomguk.kirly.ui.main
 
 import android.annotation.SuppressLint
+import android.graphics.Paint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.gomguk.kirly.data.Product
 import com.gomguk.kirly.databinding.ItemProductSmallBinding
 import com.gomguk.kirly.databinding.ItemProductVerticalBinding
+import com.gomguk.kirly.util.PriceUtil
 
 class ProductAdapter(private val viewType: Int = VIEW_TYPE_SMALL) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -56,8 +59,22 @@ class ProductAdapter(private val viewType: Int = VIEW_TYPE_SMALL) : RecyclerView
         RecyclerView.ViewHolder(binding.root) {
         fun bind(product: Product) {
             binding.nameTextView.text = product.name
-            binding.priceTextView.text = "${product.discountedPrice ?: product.originalPrice}원"
             
+            val priceInfo = PriceUtil.calculatePriceInfo(product.originalPrice, product.discountedPrice)
+            binding.priceTextView.text = priceInfo.priceText
+            
+            if (priceInfo.showDiscount) {
+                binding.discountPercentTextView.visibility = View.VISIBLE
+                binding.discountPercentTextView.text = priceInfo.discountPercentText
+                
+                binding.discountPriceTextView.visibility = View.VISIBLE
+                binding.discountPriceTextView.text = priceInfo.originalPriceText
+                binding.discountPriceTextView.paintFlags = binding.discountPriceTextView.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            } else {
+                binding.discountPercentTextView.visibility = View.GONE
+                binding.discountPriceTextView.visibility = View.GONE
+            }
+
             Glide.with(binding.imageView)
                 .load(product.image)
                 .into(binding.imageView)
@@ -68,8 +85,22 @@ class ProductAdapter(private val viewType: Int = VIEW_TYPE_SMALL) : RecyclerView
         RecyclerView.ViewHolder(binding.root) {
         fun bind(product: Product) {
             binding.nameTextView.text = product.name
-            binding.priceTextView.text = "${product.discountedPrice ?: product.originalPrice}원"
             
+            val priceInfo = PriceUtil.calculatePriceInfo(product.originalPrice, product.discountedPrice)
+            binding.priceTextView.text = priceInfo.priceText
+            
+            if (priceInfo.showDiscount) {
+                binding.discountPercentTextView.visibility = View.VISIBLE
+                binding.discountPercentTextView.text = priceInfo.discountPercentText
+                
+                binding.discountPriceTextView.visibility = View.VISIBLE
+                binding.discountPriceTextView.text = priceInfo.originalPriceText
+                binding.discountPriceTextView.paintFlags = binding.discountPriceTextView.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            } else {
+                binding.discountPercentTextView.visibility = View.GONE
+                binding.discountPriceTextView.visibility = View.GONE
+            }
+
             Glide.with(binding.imageView)
                 .load(product.image)
                 .into(binding.imageView)
