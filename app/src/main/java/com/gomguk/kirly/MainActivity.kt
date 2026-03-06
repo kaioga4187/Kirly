b479155beb030dcc70bf689929995c74482459e0
Gomguk <kaioga4187@gmail.com>
2026-03-07 00:39:10 +0900

    apply assets

--- app/src/main/java/com/gomguk/kirly/MainActivity.kt
+++ app/src/main/java/com/gomguk/kirly/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.gomguk.kirly.databinding.ActivityMainBinding
 import com.gomguk.kirly.ui.main.MainAdapter
 import com.gomguk.kirly.ui.main.MainViewModel
@@ -40,24 +41,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupRecyclerView() {
-        mainAdapter = MainAdapter(emptyList(), mainViewModel::loadSectionItems, mainViewModel::toggleFavorite)
+        mainAdapter =
+            MainAdapter(emptyList(), mainViewModel::loadSectionItems, mainViewModel::toggleFavorite)
         binding.recyclerView.apply {
             adapter = mainAdapter
             layoutManager = LinearLayoutManager(this@MainActivity)
 
-//            addOnScrollListener(object : RecyclerView.OnScrollListener() {
-//                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-//                    super.onScrolled(recyclerView, dx, dy)
-//
-//                    val layoutManager = recyclerView.layoutManager as LinearLayoutManager
-//                    val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
-//                    val totalItemCount = layoutManager.itemCount
-//
-//                    if (lastVisibleItemPosition >= totalItemCount - 3) {
-//                        mainViewModel.loadItems()
-//                    }
-//                }
-//            })
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+
+                    val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                    val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
+                    val totalItemCount = layoutManager.itemCount
+
+                    if (lastVisibleItemPosition >= totalItemCount) {
+                        mainViewModel.loadItems()
+                    }
+                }
+            })
         }
     }
 }
